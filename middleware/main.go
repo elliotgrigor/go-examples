fe9615401de1542965110a1327ba3995f3103257
elliotgrigor <elliot.grigor@pm.me>
2026-01-02 20:05:06 +0000

    updated middleware example with working error handling middleware

--- middleware/main.go
+++ middleware/main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
-func handleIndex(w http.ResponseWriter, r *http.Request) {
+func handleIndex(w http.ResponseWriter, r *http.Request) error {
+	err := errors.New("Simulated error from `handleIndex`")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, `<head><link rel="icon" href="data:,"></head><pre>Internal Server Error</pre>`)
+		return err
+	}
+
 	// <link> suppresses Chrome's double request
 	fmt.Fprintln(w, `<head><link rel="icon" href="data:,"></head><h1>Hello</h1>`)
+	return nil
 }
 
 func main() {
 	var app App
 	app.Mux = http.NewServeMux()
-	app.Mux.HandleFunc("GET /", handleIndex)
+	make(app.Mux, "GET /", handleIndex)
 
-	app.Use(logMiddleware)
-	app.Use(authMiddleware)
-	app.Use(errorHandlerMiddleware)
+	app.Use(loggerMiddleware)
+	// app.Use(authMiddleware) // removed to avoid polluting stdout
 
 	http.ListenAndServe(":3000", app.Run())
 }
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -6,7 +6,22 @@ import (
 	"net/http"
 )
 
-func logMiddleware(next http.Handler) http.Handler {
+type appHandlerFunc func(w http.ResponseWriter, r *http.Request) error
+
+func errorHandlerMiddleware(next appHandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := next(w, r); err != nil {
+			log.Println("[ERROR]", err)
+		}
+	}
+}
+
+// make wraps each route to centralise error handling
+func make(mux *http.ServeMux, pattern string, handler appHandlerFunc) {
+	mux.HandleFunc(pattern, errorHandlerMiddleware(handler))
+}
+
+func loggerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println(r.RemoteAddr, r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
@@ -16,13 +31,6 @@ func logMiddleware(next http.Handler) http.Handler {
 func authMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Auth Middleware")
-		next.ServeHTTP(w, r)
-	})
-}
-
-func errorHandlerMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Error Middleware")
 		next.ServeHTTP(w, r)
 	})
 }
