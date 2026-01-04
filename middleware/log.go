704b544b6f3f33dc8cbd40de20394254acdeef6a
elliotgrigor <elliot.grigor@pm.me>
2026-01-04 14:09:19 +0000

    moved logging output to file

--- middleware/log.go
+++ middleware/log.go
@@ -5,8 +5,14 @@ import (
 	"os"
 )
 
+const file = "app.log"
+
 var logger *slog.Logger
 
 func init() {
-	logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+	f, err := os.OpenFile(file, os.O_WRONLY|os.O_APPEND, 0664)
+	if err != nil {
+		panic(err)
+	}
+	logger = slog.New(slog.NewTextHandler(f, nil))
 }
